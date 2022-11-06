d586fc21341a99bf566ee3cff9f2565a0562c319
tumble-weed <aniketsinghresearch@gmail.com>
2022-11-06 09:49:30 +0530

    run_experiments

--- run_experiments.sh
+++ run_experiments.sh
@@ -9,12 +9,34 @@
 ####################################################################
 # Prey
 ####################################################################
-PROBLEMS=( "Prey" )
-ALGOS=( "SAC" )
-for problem in "${PROBLEMS[@]}"
-do
-	for algo in "${ALGOS[@]}"
-	do
-		CUDA_VISIBLE_DEVICES=0 python -m Experiments.Adaptive_${problem}_${algo} --n-contr-samples=10000 --n-rl-itr=40001 --log-dir=boed_results/${problem}_${algo}  --bound-type=lower --id=1 --budget=10 --tau=0.01 --pi-lr=0.0001 --qf-lr=0.001 --discount=0.95 --buffer-capacity=1000000 --temp=1 --target-entropy=2.85189124 --ens-size=10 2>&1 | tee ${problem}_${algo}.txt
-	done
-done
+#mkdir boed_results
+#PROBLEMS=( "Prey" )
+#ALGOS=( "SAC" "CES" "REM" )
+#for problem in "${PROBLEMS[@]}"
+#do
+#	for algo in "${ALGOS[@]}"
+#	do
+#		CUDA_VISIBLE_DEVICES=0 python -m Experiments.Adaptive_${problem}_${algo} --n-contr-samples=10000 --n-rl-itr=40001 --log-dir=boed_results/${problem}_${algo}  --bound-type=lower --id=1 --budget=10 --tau=0.01 --pi-lr=0.0001 --qf-lr=0.001 --discount=0.95 --buffer-capacity=1000000 --temp=1 --target-entropy=2.85189124 --ens-size=10 2>&1 | tee boed_results/${problem}_${algo}.txt
+#	done
+#done
+
+mkdir boed_results
+####################################################################
+algo="SAC"
+problem="Prey"
+CUDA_VISIBLE_DEVICES=0 python -m Experiments.Adaptive_${problem}_${algo} --n-contr-samples=10000 --n-rl-itr=40001 --log-dir=boed_results/${problem}_${algo}  --bound-type=lower --id=1 --budget=10 --tau=0.01 --pi-lr=0.0001 --qf-lr=0.001 --discount=0.95 --buffer-capacity=1000000 --temp=1 --target-entropy=2.85189124 --ens-size=10 2>&1 | tee boed_results/${problem}_${algo}.txt
+####################################################################
+# Source
+####################################################################
+algo="SAC"
+problem="Source"
+CUDA_VISIBLE_DEVICES=0 python -m Experiments.Adaptive_${problem}_${algo} --n-contr-samples=100000 --n-rl-itr=20001 --log-dir=boed_results/${problem}_${algo}  --bound-type=lower --id=1 --budget=30 --discount=0.9 --buffer-capacity=1000000 --tau=0.001 --pi-lr=0.0001 --qf-lr=0.0003 --ens-size=2 | tee boed_results/${problem}_${algo}.txt
+####################################################################
+# CES
+####################################################################
+algo="SAC"
+problem="CES"
+CUDA_VISIBLE_DEVICES=0 python -m Experiments.Adaptive_${problem}_${algo} --n-contr-samples=100000 --n-rl-itr=20001 --log-dir=boed_results/${problem}_${algo}  --bound-type=lower --id=1 --budget=10 --discount=0.9 --buffer-capacity=1000000  | tee boed_results/${problem}_${algo}.txt
+####################################################################
+
+
