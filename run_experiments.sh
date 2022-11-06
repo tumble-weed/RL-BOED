baaa8f900786f85079cc53f8fdd68ae2c551becb
tumble-weed <aniketsinghresearch@gmail.com>
2022-11-06 09:34:25 +0530

    run experiments

---
+++ run_experiments.sh
@@ -0,0 +1,20 @@
+####################################################################
+# Source
+####################################################################
+# python -m Experiments.Adaptive_Source_SAC --n-contr-samples=100000 --n-rl-itr=20001 --log-dir=boed_results/source  --bound-type=lower --id=1 --budget=30 --discount=0.9 --buffer-capacity=1000000 --tau=0.001 --pi-lr=0.0001 --qf-lr=0.0003 --ens-size=2
+####################################################################
+# CES
+####################################################################
+#CUDA_VISIBLE_DEVICES=0 python -m Experiments.Adaptive_CES_SAC --n-contr-samples=100000 --n-rl-itr=20001 --log-dir=boed_results/ces  --bound-type=lower --id=1 --budget=10 --discount=0.9 --buffer-capacity=1000000 | tee prey_ces.txt
+####################################################################
+# Prey
+####################################################################
+PROBLEMS=( "Prey" )
+ALGOS=( "SAC" )
+for problem in "${PROBLEMS[@]}"
+do
+	for algo in "${ALGOS[@]}"
+	do
+		CUDA_VISIBLE_DEVICES=0 python -m Experiments.Adaptive_${problem}_${algo} --n-contr-samples=10000 --n-rl-itr=40001 --log-dir=boed_results/${problem}_${algo}  --bound-type=lower --id=1 --budget=10 --tau=0.01 --pi-lr=0.0001 --qf-lr=0.001 --discount=0.95 --buffer-capacity=1000000 --temp=1 --target-entropy=2.85189124 --ens-size=10 2>&1 | tee ${problem}_${algo}.txt
+	done
+done
