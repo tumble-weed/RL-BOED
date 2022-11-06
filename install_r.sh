c8e1703e8a56691bf6152db32497cb0a3ae2c35e
tumble-weed <aniketsinghresearch@gmail.com>
2022-11-06 03:28:56 +0000

    install r

---
+++ install_r.sh
@@ -0,0 +1,11 @@
+sudo apt-get install dirmngr
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+
+sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran40/'
+
+sudo apt update
+
+sudo apt install r-base -y
+
+sudo -i R
